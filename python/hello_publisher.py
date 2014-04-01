d302af218e20e62d66bb54191a379e7bd6d9ae89
linliang <lin.liang@tcl.com>
2014-04-01 09:37:59 +0800

    add kombu examples

---
+++ python/hello_publisher.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from kombu import Connection
+import datetime
+
+with Connection('amqp://guest:guest@localhost:5672//') as conn:
+    simple_queue = conn.SimpleQueue('simple_queue')
+    message = 'helloword, sent at %s' % datetime.datetime.today()
+    simple_queue.put(message)
+    print('Sent: %s' % message)
+    simple_queue.close()
